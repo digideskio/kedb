e3ac6414d73629b5e402348b35de32e9dcc3114d
root <root@localhost.localdomain>
2014-05-20 15:47:21 +0200

    workarounds field

--- kedb/serialisers.py
+++ kedb/serialisers.py
@@ -3,15 +3,16 @@ from rest_framework import serializers
 
 from kedb.models import KnownError, Workaround
 
-class KnownErrorSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = KnownError
-        fields = ('id', 'name', 'description', 'check', 'output_pattern', 'level', 'severity')
-
-
 class WorkaroundSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Workaround
         fields = ('id', 'known_error', 'description', 'temporary')
+
+class KnownErrorSerializer(serializers.HyperlinkedModelSerializer):
+
+    workarounds = WorkaroundSerializer(many=True)
+
+    class Meta:
+        model = KnownError
+        fields = ('id', 'name', 'description', 'check', 'output_pattern', 'level', 'severity', 'workarounds')
