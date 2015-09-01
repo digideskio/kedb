582a81253b96906eca4e722b1b751c95a5ff3fd0
Michael Kutý <6du1ro.n@gmail.com>
2015-09-01 19:01:36 +0200

    change setup.py to PBR standard (reverted from commit 36de3ba11649a895db06694e21c566d3b5a30546)

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
+#!/usr/bin/env python
 
-import setuptools
+from distutils.core import setup
+import os
+import setuplib
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
+packages, package_data = setuplib.find_packages('kedb')
 
-setuptools.setup(
-    setup_requires=['pbr>=0.11,<2.0'],
-    pbr=True)
+setup(name='Django-kedb',
+    version=__import__('kedb').__version__,
+    description='Django-based kedb middleware.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Ales Komarek',
+    author_email='mail@newt.cz',
+    url='http://newt.cz/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=packages,
+    package_data=package_data,
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audikedbe :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+)
