fdec81a5f79ca1c14dfc565fd38553d22a75e0d6
Michael Kutý <6du1ro.n@gmail.com>
2015-09-01 19:06:19 +0200

    back to old setup.py with fixes

--- setup.py
+++ setup.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 import os
-import setuplib
 
-packages, package_data = setuplib.find_packages('kedb')
+from pip.req import parse_requirements
+from setuptools import find_packages, setup
 
-setup(name='Django-kedb',
-    version=__import__('kedb').__version__,
-    description='Django-based kedb middleware.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-    author='Ales Komarek',
-    author_email='mail@newt.cz',
-    url='http://newt.cz/',
-    license='BSD License',
-    platforms=['OS Independent'],
-    packages=packages,
-    package_data=package_data,
-    classifiers=[
-        'Development Status :: 1 - Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audikedbe :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-    ],
-)
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as fh:
+    readme = fh.read()
+
+requirements = parse_requirements(
+    os.path.join(os.path.dirname(__file__), 'requirements.txt'))
+install_requires = [str(req.req) for req in requirements]
+
+
+setup(name='django-kedb',
+      version=__import__('kedb').__version__,
+      description='Django-based kedb middleware.',
+      long_description=readme,
+      author='Ales Komarek',
+      author_email='mail@newt.cz',
+      url='http://newt.cz/',
+      license='BSD License',
+      platforms=['OS Independent'],
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      classifiers=[
+          'Development Status :: 1 - Alpha',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audikedbe :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+      ],
+      )
